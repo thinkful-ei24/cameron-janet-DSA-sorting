08f8a5efde5f45a8d520dcb07fec8dfe411c5ca8
Janet Leon <leon.irenejanet@gmail.com>
2018-11-15 13:57:08 -0800

    adds comments to quicksort

--- sortBooks.js
+++ sortBooks.js
@@ -5,6 +5,10 @@ const data = [ 'Harry Potter', 'The Lion the Witch', 'Wardrobe', 'Lord Of the Ri
 //input: [ 'Harry Potter', 'The Lion the Witch', 'Wardrobe', 'Lord Of the Rings', 'Hitch Hikers Guide to the Galaxy'];
 //output : ['Harry Potter', 'Hitch Hikers Guide to the Galaxy', 'Lord Of the Rings', 'The Lion the Witch', 'Wardrobe' ]
 
+//Since we can compare strings in javascript 
+//We can run this array through a quick sort because every string of lesser value will be on the left
+//A string of greater value will be on the right
+//When quick sort resolves will have an order of least to greatest from left to right
 console.log('Harry Potter'>'The Lion the Witch');
 
 //see quickSort.js
